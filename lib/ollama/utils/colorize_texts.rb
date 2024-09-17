a4ce6f14a71ad96ce482a993e4043755a8e27b91
Florian Frank <flori@ping.de>
2024-09-17 10:44:31 +0200

    Add comments to ColorizeTexts utility class

--- lib/ollama/utils/colorize_texts.rb
+++ lib/ollama/utils/colorize_texts.rb
@@ -3,11 +3,20 @@ class Ollama::Utils::ColorizeTexts
   include Term::ANSIColor
   include Ollama::Utils::Width
 
+  # Initializes a new instance of Ollama::Utils::ColorizeTexts
+  #
+  # @param [Array<String>] texts the array of strings to be displayed with colors
+  #
+  # @return [Ollama::Utils::ColorizeTexts] an instance of Ollama::Utils::ColorizeTexts
   def initialize(*texts)
-    texts = texts.map(&:to_a)
+    texts  = texts.map(&:to_a)
     @texts = Array(texts.flatten)
   end
 
+  # Returns a string representation of the object, including all texts content,
+  # colored differently and their sizes.
+  #
+  # @return [String] The formatted string.
   def to_s
     result = +''
     @texts.each_with_index do |t, i|
@@ -22,6 +31,14 @@ class Ollama::Utils::ColorizeTexts
 
   private
 
+  # Returns the nearest RGB color to the given ANSI color
+  #
+  # @param [color] color The ANSI color attribute
+  #
+  # @return [Array<RGBTriple>] An array containing two RGB colors, one for black and
+  #   one for white text, where the first is the closest match to the input color
+  #   when printed on a black background, and the second is the closest match
+  #   when printed on a white background.
   def text_color(color)
     color = Term::ANSIColor::Attribute[color]
     [
@@ -30,6 +47,9 @@ class Ollama::Utils::ColorizeTexts
     ].max_by { |t| t.distance_to(color) }
   end
 
+  # Returns an array of colors for each step in the gradient
+  #
+  # @return [Array<Array<Integer>>] An array of RGB color arrays
   def colors
     @colors ||= (0..255).map { |i|
       [
