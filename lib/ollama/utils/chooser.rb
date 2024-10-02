979e556a5c616d7d25536f3c51c57cb34a1e91e5
Florian Frank <flori@ping.de>
2024-10-02 17:14:23 +0200

    Add prompt to choose method
    - Added `prompt` parameter to `choose` method in `Ollama::Utils::Chooser`
    - Modified output formatting for selected entry in `choose` method
    - Updated `choose` method to handle cases better where no entry was chosen

--- lib/ollama/utils/chooser.rb
+++ lib/ollama/utils/chooser.rb
@@ -8,8 +8,9 @@ module Ollama::Utils::Chooser
 
   module_function
 
-  def choose(entries)
+  def choose(entries, prompt: 'Search? %s')
     entry = Search.new(
+      prompt:,
       match: -> answer {
         matcher = Amatch::PairDistance.new(answer.downcase)
         matches = entries.map { |n| [ n, -matcher.similar(n.to_s.downcase) ] }.
@@ -19,7 +20,7 @@ module Ollama::Utils::Chooser
       },
       query: -> _answer, matches, selector {
         matches.each_with_index.map { |m, i|
-          i == selector ? "#{blue{?⮕}} #{on_blue{m}}" : "  #{m}"
+          i == selector ? "#{blue{?⮕}} #{on_blue{m}}" : "  #{m.to_s}"
         } * ?\n
       },
       found: -> _answer, matches, selector {
@@ -27,6 +28,11 @@ module Ollama::Utils::Chooser
       },
       output: STDOUT
     ).start
-    return entry if entry
+    if entry
+      entry
+    else
+      print clear_screen, move_home
+      nil
+    end
   end
 end
