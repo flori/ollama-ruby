84617ab13bde966793ac36bb813d4350feafc0ba
Florian Frank <flori@ping.de>
2024-11-02 16:59:03 +0100

    Update Ollama::Handlers::Say to accept interactive mode
    * Update `initialize` method in lib/ollama/handlers/say.rb to add
      `interactive` option and call new `command` method.
    * Add private `command` method in lib/ollama/handlers/say.rb to generate
      command for say utility based on voice and interactive options.
    * Update specs in spec/ollama/handlers/say_spec.rb to test new behavior.

--- lib/ollama/handlers/say.rb
+++ lib/ollama/handlers/say.rb
@@ -3,8 +3,8 @@ require 'shellwords'
 class Ollama::Handlers::Say
   include Ollama::Handlers::Concern
 
-  def initialize(output: nil, voice: 'Samantha')
-    output ||= IO.popen(Shellwords.join([ 'say', '-v', voice ]), 'w')
+  def initialize(output: nil, voice: 'Samantha', interactive: nil)
+    output ||= IO.popen(Shellwords.join(command(voice:, interactive:)), 'w')
     super(output:)
     @output.sync = true
   end
@@ -15,5 +15,19 @@ class Ollama::Handlers::Say
     end
     response.done and @output.close
     self
+  end
+
+  private
+
+  def command(voice:, interactive:)
+    command = [ 'say' ]
+    voice and command.concat([ '-v', voice ])
+    case interactive
+    when true
+      command << '-i'
+    when String
+      command << '--interactive=%s' % interactive
+    end
+    command
   end
 end
--- spec/ollama/handlers/say_spec.rb
+++ spec/ollama/handlers/say_spec.rb
@@ -1,30 +1,79 @@
 require 'spec_helper'
 
 RSpec.describe Ollama::Handlers::Say do
+  let :say do
+    described_class.new
+  end
+
   it 'has .to_proc' do
     expect_any_instance_of(described_class).to receive(:call).with(:foo)
     described_class.call(:foo)
   end
 
-  it 'can print response' do
-    output = double('output', :sync= => true)
-    expect(output).to receive(:print).with('testing')
-    expect(output).to receive(:close)
-    print = described_class.new(output:)
-    response = double('response', response: 'testing', done: false)
-    print.call(response)
-    response = double('response', response: nil, message: nil, done: true)
-    print.call(response)
+  it 'can be instantiated' do
+    expect(say).to be_a described_class
   end
 
-  it 'can print message content' do
+  it 'can be instantiated with a given voice' do
+    expect_any_instance_of(described_class).to receive(:command).
+      with(hash_including(voice: 'TheVoice')).and_return %w[ true ]
+    say = described_class.new(voice: 'TheVoice')
+    expect(say).to be_a described_class
+  end
+
+  describe 'command' do
+    it 'can be instantiated interactively' do
+      expect_any_instance_of(described_class).to receive(:command).
+        with(hash_including(interactive: true)).and_return %w[ true ]
+      say = described_class.new(interactive: true)
+      expect(say).to be_a described_class
+    end
+
+    it 'can set the voice' do
+      expect(say.send(:command, voice: 'TheVoice', interactive: nil)).to eq(
+        %w[ say -v TheVoice ]
+      )
+    end
+
+    it 'can be instantiated interactively with green' do
+      expect_any_instance_of(described_class).to receive(:command).
+        with(hash_including(interactive: 'green')).and_return %w[ true ]
+      say = described_class.new(interactive: 'green')
+      expect(say).to be_a described_class
+    end
+
+    it 'can set interactive mode' do
+      expect(say.send(:command, voice: nil, interactive: true)).to eq(
+        %w[ say -i ]
+      )
+    end
+
+    it 'can set interactive mode to green' do
+      expect(say.send(:command, voice: nil, interactive: 'green')).to eq(
+        %w[ say --interactive=green ]
+      )
+    end
+  end
+
+  it 'can say response' do
     output = double('output', :sync= => true)
     expect(output).to receive(:print).with('testing')
     expect(output).to receive(:close)
-    print = described_class.new(output:)
-    response = double('response', response: nil, message: double(content: 'testing'), done: false)
-    print.call(response)
+    say = described_class.new(output:)
+    response = double('response', response: 'testing', done: false)
+    say.call(response)
     response = double('response', response: nil, message: nil, done: true)
-    print.call(response)
+    say.call(response)
+  end
+
+  it 'can say message content' do
+    output = double('output', :sync= => true)
+    expect(output).to receive(:print).with('testing')
+    expect(output).to receive(:close)
+    say = described_class.new(output:)
+    response = double('response', response: nil, message: double(content: 'testing'), done: false)
+    say.call(response)
+    response = double('response', response: nil, message: nil, done: true)
+    say.call(response)
   end
 end
