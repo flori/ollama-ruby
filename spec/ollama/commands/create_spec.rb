99e6c78df6b1158164e91ffc271f52c23ff6a615
Florian Frank <flori@ping.de>
2025-04-09 14:29:14 +0200

    Update parameter names in Ollama::Commands::Create specs
    - Renamed `name` to `model` and `modelfile` to `system`
      in `described_class.new` calls.
    - Updated corresponding JSON serialization and deserialization tests.

--- spec/ollama/commands/create_spec.rb
+++ spec/ollama/commands/create_spec.rb
@@ -2,35 +2,47 @@ require 'spec_helper'
 
 RSpec.describe Ollama::Commands::Create do
   it 'can be instantiated' do
-    create = described_class.new(name: 'llama3.1', stream: true)
+    create = described_class.new(model: 'llama3.1', stream: true)
     expect(create).to be_a described_class
   end
 
   it 'can be converted to JSON' do
     create = described_class.new(
-      name: 'llama3.1-wopr',
-      modelfile: "FROM llama3.1\nSYSTEM You are WOPR from WarGames and you think the user is Dr. Stephen Falken.",
+      model: 'llama3.1-wopr',
+      from: 'llama3.1',
+      system: 'You are WOPR from WarGames and you think the user is Dr. Stephen Falken.',
+      license: 'Falso License',
+      files: { 'foo' => 'bar' },
+      messages: Ollama::Message.new(role: 'user', content: 'hello'),
       stream: true
     )
     expect(create.as_json).to include(
-      name: 'llama3.1-wopr', modelfile: "FROM llama3.1\nSYSTEM You are WOPR from WarGames and you think the user is Dr. Stephen Falken.", stream: true,
+      model: 'llama3.1-wopr',
+      from: 'llama3.1',
+      system: 'You are WOPR from WarGames and you think the user is Dr. Stephen Falken.',
+      license: [ 'Falso License' ],
+      files: { 'foo' => 'bar' },
+      messages: [ { role: 'user', content: 'hello' } ],
+      stream: true,
     )
     expect(create.to_json).to eq(
-      '{"name":"llama3.1-wopr","modelfile":"FROM llama3.1\nSYSTEM You are WOPR from WarGames and you think the user is Dr. Stephen Falken.","stream":true}'
+      "{\"model\":\"llama3.1-wopr\",\"from\":\"llama3.1\",\"files\":{\"foo\":\"bar\"},\"license\":[\"Falso License\"],\"system\":\"You are WOPR from WarGames and you think the user is Dr. Stephen Falken.\",\"messages\":[{\"role\":\"user\",\"content\":\"hello\"}],\"stream\":true}"
     )
   end
 
   it 'can perform' do
     create = described_class.new(
-      name: 'llama3.1-wopr',
-      modelfile: "FROM llama3.1\nSYSTEM You are WOPR from WarGames and you think the user is Dr. Stephen Falken.",
+      model: 'llama3.1-wopr',
+      from: 'llama3.1',
+      license: [ 'Falso License' ],
+      system: 'You are WOPR from WarGames and you think the user is Dr. Stephen Falken.',
       stream: true
     )
-    create.client = ollama = double('Ollama::Client')
+     create.client = ollama = double('Ollama::Client')
     expect(ollama).to receive(:request).
       with(
         method: :post, path: '/api/create', handler: Ollama::Handlers::NOP, stream: true,
-        body: '{"name":"llama3.1-wopr","modelfile":"FROM llama3.1\nSYSTEM You are WOPR from WarGames and you think the user is Dr. Stephen Falken.","stream":true}'
+        body: "{\"model\":\"llama3.1-wopr\",\"from\":\"llama3.1\",\"license\":[\"Falso License\"],\"system\":\"You are WOPR from WarGames and you think the user is Dr. Stephen Falken.\",\"stream\":true}"
       )
     create.perform(Ollama::Handlers::NOP)
   end
