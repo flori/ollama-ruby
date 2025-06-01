004f75bb77e1918ebf325988f6bc70fa003f4974
Florian Frank <flori@ping.de>
2025-06-01 02:56:58 +0200

    Updated Message class to include new `tool_calls` attribute.
    - Added `tool_calls` parameter to the `initialize` method.
    - Updated instance variable assignment in the `initialize` method.

--- lib/ollama/message.rb
+++ lib/ollama/message.rb
@@ -3,8 +3,9 @@ class Ollama::Message
 
   attr_reader :role, :content, :thinking, :images
 
-  def initialize(role:, content:, thinking: nil, images: nil, **)
-    @role, @content, @thinking, @images  =
-      role, content, thinking, (Array(images) if images)
+  def initialize(role:, content:, thinking: nil, images: nil, tool_calls: nil, **)
+    @role, @content, @thinking, @images, @tool_calls =
+      role, content, thinking, (Array(images) if images),
+      (Array(tool_calls) if tool_calls)
   end
 end
